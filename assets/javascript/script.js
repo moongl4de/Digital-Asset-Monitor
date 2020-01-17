b953fed782a6dfb1b51560fca18e26f32f2cd0d4
alexgignilliat <56325924+alexgignilliat@users.noreply.github.com>
2020-01-16 21:41:54 -0500

    added placeholder article links

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,6 +6,7 @@ var newsKey = "5d34d4f133f84cbcba757cdb7cd169ef";
 
 var modal = $("#main-modal")
 var xButton = $("#x-button")
+var modalTitle = $(".modal-card-title")
 
 read();
 
@@ -17,30 +18,33 @@ function read() {
       }
       $("#header-reader").text(buzzwordsSplit[i]);
       i++;
-    }, 1250);
-  }
-
-  //test
+    }, 500);
+  };
 
 
+  //Ideally, we should be able to have one or two functions. One to interact with the news API and print that information into the modal, and one to interact with the Nomics API to grab price, market cap, volume, etc.
 
   xButton.on('click', function(){
     modal.removeClass("is-active")
-  })
+    modalTitle.html("")
+  });
 
 
   $("#btc-icon").on('click', function(){
     console.log("Testing BTC")
     modal.addClass("is-active")
+    modalTitle.html("Bitcoin")
     
-  })
+  });
 
   $("#eth-icon").on('click', function(){
     modal.addClass("is-active")
-  })
+    modalTitle.html("Ethereum")
+  });
 
   $("#xrp-icon").on('click', function(){
     modal.addClass("is-active")
-  })  
+    modalTitle.html("XRP")
+  });  
 
   
